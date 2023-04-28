7a7d9ec4ee4e2b9814df9c92405c8f67d565f0de
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-04-28 10:19:12 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.15 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,22 +1,22 @@
 class Solution {
     public int solution(int num) {
-        long n = num;
+        
         int answer = 0;
-        while (n != 1) {
-            if(n % 2 == 0) {
-                n /= 2;
+        while (num > 1) {
+            if(num % 2 == 0) {
+                num /= 2;
             } else {
-                n *= 3;
-                n += 1;
+                num *= 3;
+                num += 1;
             }
             
             answer++;
             
+            
         }
-        if (answer >= 500) {
-            answer = -1;
-        }
+        System.out.println(answer);
+        
 
-        return answer;
+        return num == 1 ? answer : -1;
     }
 }
