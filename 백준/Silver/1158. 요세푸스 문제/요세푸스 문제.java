fc7511a1c5f8b1eb10a2d7afc19f51259c570780
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-04-28 13:30:04 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 552 ms, Memory: 294724 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,32 +1,36 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static int N; // 1번부터 N번까지 N명의 사람
-    public static int K; // 순서대로 K번째 사람을 제거한다.
+    public static int n, k;
     public static LinkedList<Integer> list = new LinkedList<>();
     public static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        sb.append("<");
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        
-        for (int i = 1; i <= N; i++) {
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= n; i++) {
             list.offer(i);
         }
-        
-        
+
+        sb.append("<");
+
         while (list.size() > 1) {
-            for (int i = 0; i < K - 1; i++) {
-                int num = list.poll();
-                
-                list.offer(num);
+            for (int i = 0; i < k - 1; i++) {
+                list.offer(list.poll());
             }
+
             sb.append(list.poll()).append(", ");
         }
+
         sb.append(list.poll()).append(">");
+
         System.out.println(sb);
-                
     }
 }
