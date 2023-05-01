deda74b1b0132f926325a8e33196fa9a52972a8e
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-05-01 10:56:13 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -4,33 +4,12 @@ class Solution {
         // 1. 세 숫자가 모두 다른 경우
         if (a != b && b != c) {
             answer = a + b + c;
-        } else if (a == b && b == c) {
-            int sum = a + b + c;
-            int sum2 = (int) Math.pow(a, 2);
-            sum2 += (int) Math.pow(b, 2);
-            sum2 += (int) Math.pow(c, 2);
-            int sum3 = (int) Math.pow(a, 3);
-            sum3 += (int) Math.pow(b, 3);
-            sum3 += (int) Math.pow(c, 3);
-            answer = sum * sum2 * sum3;
         } 
-        // else {
-        //     int sum = a + b + c;
-        //     System.out.println("a: " + a);
-        //     System.out.println("b: " + b);
-        //     System.out.println("c: " + c);
-        //     int sum2 = (int) Math.pow(a, 2);
-        //     sum2 += (int) Math.pow(b, 2);
-        //     sum2 += (int) Math.pow(c, 2);
-        //     answer = sum * sum2;
-        // }
         
         // 2. 두 숫자만 같을 때
         if ((a == b && b != c) || (b == c && a != c) || (a == c && a != b)) {
             int sum = a + b + c;
-        //     System.out.println("a: " + a);
-        //     System.out.println("b: " + b);
-        //     System.out.println("c: " + c);
+        
             int sum2 = (int) Math.pow(a, 2);
             sum2 += (int) Math.pow(b, 2);
             sum2 += (int) Math.pow(c, 2);
@@ -39,17 +18,17 @@ class Solution {
         
         
         // 3. 세 숫자가 모두 같을 때
-//         if (a == b && b == c) {
+        if (a == b && b == c) {
             
-//             int sum = a + b + c;
-//             int sum2 = (int) Math.pow(a, 2);
-//             sum2 += (int) Math.pow(b, 2);
-//             sum2 += (int) Math.pow(c, 2);
-//             int sum3 = (int) Math.pow(a, 3);
-//             sum3 += (int) Math.pow(b, 3);
-//             sum3 += (int) Math.pow(c, 3);
-//             answer = sum * sum2 * sum3;
-//         }
+            int sum = a + b + c;
+            int sum2 = (int) Math.pow(a, 2);
+            sum2 += (int) Math.pow(b, 2);
+            sum2 += (int) Math.pow(c, 2);
+            int sum3 = (int) Math.pow(a, 3);
+            sum3 += (int) Math.pow(b, 3);
+            sum3 += (int) Math.pow(c, 3);
+            answer = sum * sum2 * sum3;
+        }
         return answer;
     }
 }
