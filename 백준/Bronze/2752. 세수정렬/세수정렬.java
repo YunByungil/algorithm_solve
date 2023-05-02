3bd28ea229749b964fc1e4a5747141bde16038a5
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-05-02 09:57:14 +0900

    [Bronze IV] Title: 세수정렬, Time: 144 ms, Memory: 16028 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int[] arr = new int[3];
+        
+        for (int i = 0; i < arr.length; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            arr[i] = num;
+        }
+        
+        Arrays.sort(arr);
+        
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
