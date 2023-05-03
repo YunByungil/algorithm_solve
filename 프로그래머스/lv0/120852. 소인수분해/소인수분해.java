4513823b038522b3fa01946957afe68259cbbb6b
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-05-03 14:53:27 +0900

    [level 0] Title: 소인수분해, Time: 4.77 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -6,18 +6,18 @@ class Solution {
         
         for (int i = 2; i <= n; i++) {
             if (n % i == 0) {
-                System.out.println(i);
+                // System.out.println(i);
                 set.add(i);
                 n /= i;
                 i--;
             }
             
         }
-        answer = new int[set.size()];
-        int count = 0;
-        for (Integer s : set) {
-            answer[count++] = s;
-        }
-        return answer;
+        // answer = new int[set.size()];
+        // int count = 0;
+        // for (Integer s : set) {
+        //     answer[count++] = s;
+        // }
+        return set.stream().mapToInt(Integer::intValue).toArray();
     }
 }
