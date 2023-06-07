76b18278b08986f976a124993ce695e3942ccbf0
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-06-07 17:31:15 +0900

    [Silver I] Title: 곱셈, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,34 +1,30 @@
-// "static void main" must be defined in a public class.
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 public class Main {
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        // a를 b번 곱한 수 이를 c로 나눈 나머지를 구하는 프로그램
+        
         long a = Long.parseLong(st.nextToken());
         long b = Long.parseLong(st.nextToken());
         long c = Long.parseLong(st.nextToken());
         
-        System.out.println(func(a, b, c));
-        
+        System.out.println(dfs(a, b, c));
     }
     
-    public static long func(long a, long b, long c) {
+    public static long dfs(long a, long b, long c) {
         if (b == 1) {
             return a % c;
         }
         
-        long val = func(a, b / 2, c);
-        // System.out.println(val);
+        long val = dfs(a, b / 2, c);
         val = val * val % c;
         
-         if (b % 2 == 0) {
-             return val;
-         }
-        // System.out.println(val * a % c);
+        if (b % 2 == 0) {
+            return val;
+        }
         return val * a % c;
     }
-    
 }
