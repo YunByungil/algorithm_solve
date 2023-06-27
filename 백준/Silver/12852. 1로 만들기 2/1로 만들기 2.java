1269728aaba7d4ba151d272bd125a06d670ac853
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-06-27 16:11:47 +0900

    [Silver I] Title: 1로 만들기 2, Time: 168 ms, Memory: 22400 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        int[] arr = new int[n + 1];
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            arr[i] = i - 1;
+            
+            if (i % 2 == 0) {
+                if (dp[i / 2] + 1 < dp[i]) {
+                    dp[i] = dp[i / 2] + 1;
+                    arr[i] = i / 2;
+                }
+            }
+            
+            if (i % 3 == 0) {
+                if (dp[i / 3] + 1 < dp[i]) {
+                    dp[i] = dp[i / 3] + 1;
+                    arr[i] = i / 3;
+                }
+            }
+        }
+        
+        System.out.println(dp[n]);
+        
+        while (n > 0) {
+            System.out.print(n + " ");
+            n = arr[n];
+        }
+        
+    }
+}
