01a6e9c7202a424d35406cdecaabd29be1eea390
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-07-17 12:20:02 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            arr[i] = i;
+        }
+        
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            for (int j = i; j <= n; j += i) {
+                if (arr[j] == 0) {
+                    continue;
+                }
+                arr[j] = 0;
+                count++;
+                if (k == count) {
+                    System.out.println(j);
+                }
+            }
+        }
+    }
+}
