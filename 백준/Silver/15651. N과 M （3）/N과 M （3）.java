b7a4ba5991756f0e252e381723933a716900ad80
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-07-19 09:38:05 +0900

    [Silver III] Title: N과 M (3), Time: 456 ms, Memory: 68516 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,41 +1,37 @@
-// "static void main" must be defined in a public class.
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static int n, m;
     public static int[] arr;
-    public static boolean[] visit;
     public static StringBuilder sb = new StringBuilder();
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        
+
         arr = new int[m];
-        visit = new boolean[n];
-        
+
         dfs(0);
         System.out.println(sb);
     }
-    
+
     public static void dfs(int depth) {
         if (depth == m) {
-            for (int i = 0; i < m; i++) {
-                sb.append(arr[i]).append(" ");
+            for (int i : arr) {
+                sb.append(i).append(" ");
             }
             sb.append("\n");
             return;
         }
-        
+
         for (int i = 0; i < n; i++) {
             arr[depth] = i + 1;
             dfs(depth + 1);
         }
-        
     }
-    
 }
