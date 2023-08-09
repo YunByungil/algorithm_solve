3d9b30919c7eeef376239341dd5b4fb05e59cab2
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-08-09 16:09:00 +0900

    [Gold IV] Title: 부분합, Time: 284 ms, Memory: 22472 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -9,7 +9,7 @@ public class Main {
         
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
         int[] arr = new int[n];
         
         st = new StringTokenizer(br.readLine());
@@ -17,31 +17,30 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         // Arrays.sort(arr);
-        int length = 0;
+        
         int answer = Integer.MAX_VALUE;
-        int num = arr[0];
         int end = 0;
+        int num = arr[0];
         
         for (int start = 0; start < n; start++) {
-            while (end < n && num < m) {
+            while (end < n && num < s) {
                 end++;
                 if (end != n) {
                     num += arr[end];
                 }
             }
+            
             if (end == n) {
                 break;
             }
-
-            answer = Math.min(answer, (end - start) + 1);
             
+            answer = Math.min(answer, (end - start) + 1);
             num -= arr[start];
         }
         
         if (answer == Integer.MAX_VALUE) {
-            System.out.println(0);
-        } else {
-            System.out.println(answer);
+            answer = 0;
         }
+        System.out.println(answer);
     }
 }
