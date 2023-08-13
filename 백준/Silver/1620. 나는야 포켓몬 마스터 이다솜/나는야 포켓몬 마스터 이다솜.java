b1e82ae6201ca12e3b65c300663b73a7383e3596
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-08-13 11:59:51 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 528 ms, Memory: 49072 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,32 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
-        Map<String, String> dictionary = new LinkedHashMap<>();
+        String[] arr = new String[n + 1];
+        Map<String, Integer> map = new HashMap<>();
 
         for (int i = 1; i <= n; i++) {
-            st = new StringTokenizer(br.readLine());
-            String name = st.nextToken();
-            dictionary.put(name, String.valueOf(i));
-            dictionary.put(String.valueOf(i), name);
+            String name = br.readLine();
+            map.put(name, i);
+            arr[i] = name;
         }
 
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            String find = st.nextToken();
-            sb.append(dictionary.get(find)).append("\n");
+        for (int i = 1; i <= m; i++) {
+            String input = br.readLine();
+            if (isNum(input)) {
+                sb.append(arr[Integer.parseInt(input)]).append("\n");
+            } else {
+                sb.append(map.get(input)).append("\n");
+            }
         }
+
         System.out.println(sb);
+    }
+
+    public static boolean isNum(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
