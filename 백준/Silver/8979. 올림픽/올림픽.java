2af42bdaa9bb7d7e064c4559004a1d86fcaaf1c4
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-08-21 18:54:21 +0900

    [Silver V] Title: 올림픽, Time: 120 ms, Memory: 14032 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -27,29 +27,27 @@ public class Main {
         Arrays.sort(arr, new Comparator<int[]>() {
             @Override
             public int compare(final int[] o1, final int[] o2) {
-                if (o1[1] == o2[1]) {
-
-                    return o2[2] - o1[2];
-                } else {
-                    return o2[1] - o1[1];
+                if (o1[1] < o2[1]) {
+                    return o1[1] - o2[1];
                 }
+                else if (o1[1] == o2[1]) {
+                    if (o1[2] < o2[2]) {
+                        return o1[2] - o2[2];
+                    } else if (o1[2] == o2[2]) {
+                        if (o1[3] < o2[3]) {
+                            return o1[3] - o2[3];
+                        } else if (o1[3] == o2[3]) {
+                            if (o1[0] > o2[1]) {
+                                return o2[1] - o1[0];
+                            }
+                        }
+                    }
+                }
+                return o1[1] - o2[1];
             }
         });
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(final int[] o1, final int[] o2) {
-                if (o1[2] == o2[2]) {
-                    return o2[3] - o1[3];
-                } else {
-                    return o2[2] - o1[2];
-                }
-            }
-        });
-
-        int rank = 1;
-        int count = 0;
-        arr[0][4] = rank;
+        arr[0][4] = 1;
         for (int i = 1; i < n; i++) {
             if (arr[i][1] == arr[i - 1][1]
                     && arr[i][2] == arr[i - 1][2]
@@ -59,6 +57,13 @@ public class Main {
                 arr[i][4] = i + 1;
             }
         }
+
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < 4; j++) {
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
 
         for (int i = 0; i < n; i++) {
             if (arr[i][0] == k) {
