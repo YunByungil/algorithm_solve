16f0a6fc043dd414741557f72beceba72a6bde10
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-09-22 12:25:53 +0900

    [Bronze IV] Title: 주사위 세개, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,22 +1,22 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
-        
+
         int answer = 0;
         int m1 = Math.max(a, b);
         int m2 = Math.max(b, c);
         int max = Math.max(m1, m2);
-        
+
         if (a == b && b == c) {
-            
+
             answer = 10_000 + (a * 1000);
         } else if (a != b && b != c && a != c) {
             answer = max * 100;
@@ -29,7 +29,7 @@ public class Main {
                 answer = 1_000 + (a * 100);
             }
         }
-        
+
         System.out.println(answer);
     }
 }
