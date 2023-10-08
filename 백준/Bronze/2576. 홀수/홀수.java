9b2fab4d270a8b28add8c2ec81a22920a2c94e32
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-10-08 11:32:25 +0900

    [Bronze III] Title: 홀수, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Bronze/2576. 홀수/홀수.java
+++ 백준/Bronze/2576. 홀수/홀수.java
@@ -1,26 +1,29 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        
         int sum = 0;
         int min = Integer.MAX_VALUE;
         
         for (int i = 0; i < 7; i++) {
-            int input = Integer.parseInt(br.readLine());
-            
-            if (input % 2 != 0) {
-                sum += input;
-                min = Math.min(min, input);
+            int num = Integer.parseInt(br.readLine());
+            if (num % 2 != 0) {
+                sum += num;
+                min = Math.min(min, num);
             }
         }
         
-        if (sum == 0) {
+        if (min == Integer.MAX_VALUE) {
             System.out.println(-1);
         } else {
             System.out.println(sum);
             System.out.println(min);
         }
+        
+        
     }
 }
