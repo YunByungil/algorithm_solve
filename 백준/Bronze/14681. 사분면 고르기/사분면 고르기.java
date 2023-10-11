f249e502861be3cb11675bfe96fd67a676c8e7e3
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-10-11 17:36:21 +0900

    [Bronze V] Title: 사분면 고르기, Time: 120 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -1,27 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int x = Integer.parseInt(br.readLine());
+        StringTokenizer st = null;
+        
+        int x = Integer.parseInt(br.readLine());        
         int y = Integer.parseInt(br.readLine());
-
+        
         if (x > 0 && y > 0) {
             System.out.println(1);
-        }
-
-        if (x > 0 && y < 0) {
-            System.out.println(4);
-        }
-
-        if (x < 0 && y > 0) {
+        } else if (x < 0 && y > 0) {
             System.out.println(2);
-        }
-
-        if (x < 0 && y < 0) {
+        } else if (x < 0 && y < 0) {
             System.out.println(3);
+        } else if (x > 0 && y < 0) {
+            System.out.println(4);
         }
     }
 }
