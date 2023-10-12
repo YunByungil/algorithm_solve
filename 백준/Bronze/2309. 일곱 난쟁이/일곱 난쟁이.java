2e296c1104b3cf3b6210d4ce4c5ef1569a9f8356
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-10-12 20:22:29 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -2,41 +2,42 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        
         int[] arr = new int[9];
         int sum = 0;
-        int find = 0;
         for (int i = 0; i < 9; i++) {
             arr[i] = Integer.parseInt(br.readLine());
             sum += arr[i];
         }
-        
         Arrays.sort(arr);
         boolean doing = true;
         int x = 0;
         int y = 0;
         for (int i = 0; i < 8; i++) {
-            int result = 0;
             if (!doing) {
                 break;
             }
             for (int j = i + 1; j < 9; j++) {
-                result = sum - (arr[i] + arr[j]);
-                if (result == 100) {
-                    doing = false;
+                sum -= (arr[i] + arr[j]);
+                if (sum == 100) {
                     x = i;
                     y = j;
+                    doing = false;
+                    break;
+                } else {
+                    sum += (arr[i] + arr[j]);
                 }
             }
         }
         
-        // 7, 8, 10, 13, 15, 19, 20, 23, 25
         for (int i = 0; i < 9; i++) {
-            if (i != x && i != y) {
-                System.out.println(arr[i]);
+            if (i == x || i == y) {
+                continue;
             }
+            System.out.println(arr[i]);
         }
     }
 }
