4de8b8c1e92b287b266a7958ddfb9f36f65a828b
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-10-17 09:30:48 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 212 ms, Memory: 14868 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
