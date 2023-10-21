e7e66004852e7ad20eb561399176d710edefd43a
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-10-21 12:42:57 +0900

    [Silver V] Title: 방 번호, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,35 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static int[] arr = new int[10];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String n = br.readLine();
-
-        for (int i = 0; i < n.length(); i++) {
-            int num = n.charAt(i) - '0';
-            arr[num]++;
+        StringTokenizer st = null;
+        
+        // 한 세트는 0번부터 9번까지 숫자가 하나씩 들어있다.
+        
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[10];
+        
+        while (n > 0) {
+            int num = n % 10;
+            if (n % 10 == 6) {
+                arr[9]++;
+            } else {
+                arr[n % 10]++;    
+            }
+            n /= 10;
         }
-        int needNum = 0;
-        int needNine = 0;
-        int needSix = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 0) {
-                if (i == 9) {
-                    needNine = arr[i];
-                } else if (i == 6) {
-                    needSix = arr[i];  
-                } else {
-                    needNum = Math.max(needNum, arr[i]);
-                }
+        
+        int max = Integer.MIN_VALUE;
+        if (arr[9] % 2 == 0) {
+            arr[9] = arr[9] / 2;
+        } else {
+            arr[9] = arr[9] / 2 + 1;
+        }
+        
+        for (int i : arr) {
+            if (i > 0) {
+                max = Math.max(max, i);
             }
         }
-        int need = (int) Math.ceil((needNine + needSix) / 2.0);
-
-        int result = Math.max(need, needNum);
-        System.out.println(result);
+        
+        System.out.println(max);
     }
 }
