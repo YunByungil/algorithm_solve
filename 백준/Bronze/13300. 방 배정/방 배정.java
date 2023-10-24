c73b81253d240e38c086a2e79d2af7bc18ac8c9d
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-10-24 18:25:44 +0900

    [Bronze II] Title: 방 배정, Time: 136 ms, Memory: 14556 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.java
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -1,50 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static int n; // 참가하는 학생 수
-    public static int k; // 한 방에 배정할 수 있는 최대 인원 수
-    public static int s; // 학생의 성별 => 여자 0, 남자 1
-    public static int y; // 학생의 학년
-    public static int result; // 필요한 방의 개수
-    public static int[][] arr; // 학생 학년, 성별을 담는 배열
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-
-        arr = new int[7][2];
-
+        StringTokenizer st = null;
+        
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        int[][] arr = new int[7][2];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            s = Integer.parseInt(st.nextToken());
-            y = Integer.parseInt(st.nextToken());
-
+            int s = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
             arr[y][s]++;
-
         }
-
-
-        for (int i = 1; i < arr.length; i++) {
+        
+        int answer = 0;
+        int zero = 0;
+        
+        for (int i = 1; i <= 6; i++) {
             for (int j = 0; j < 2; j++) {
                 if (arr[i][j] > 0) {
                     if (arr[i][j] >= k) {
-                        result += arr[i][j] / k;
+                        answer += arr[i][j] / k;
                         if (arr[i][j] % k >= 1) {
-                            result += arr[i][j] % k;
+                            answer ++;
                         }
                     } else {
-                        result++;
+                        answer++;
                     }
+//                     if (arr[i][j] <= k) {
+//                         answer++;
+//                     }
+
+//                     if (arr[i][j] > k) {
+//                         answer += (arr[i][j] + 1) / k;
+//                     }
                 }
             }
         }
-
-        System.out.println(result);
-
+        
+        System.out.println(answer);
+        
     }
 }
