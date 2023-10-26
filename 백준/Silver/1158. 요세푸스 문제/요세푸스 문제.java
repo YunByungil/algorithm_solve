4f6f354a7598e59f0919cd8f2a1f01d8ff7a1f54
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-10-26 16:25:45 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 588 ms, Memory: 296116 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,36 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static int n, k;
-    public static LinkedList<Integer> list = new LinkedList<>();
-    public static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-
+        StringTokenizer st = null;
+        
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        
+        Queue<Integer> list = new LinkedList<>();
         for (int i = 1; i <= n; i++) {
             list.offer(i);
         }
-
-        sb.append("<");
-
+        
+        List<Integer> answer = new ArrayList<>();
         while (list.size() > 1) {
             for (int i = 0; i < k - 1; i++) {
-                list.offer(list.poll());
+                int out = list.poll();
+                list.offer(out);
             }
-
-            sb.append(list.poll()).append(", ");
+            
+            int result = list.poll();
+            sb.append(result).append(", ");
+            answer.add(result);
         }
-
-        sb.append(list.poll()).append(">");
-
-        System.out.println(sb);
+        int lastNumber = list.poll();
+        sb.append(lastNumber).append(">");
+        answer.add(lastNumber);
+        
+        System.out.println(sb);        
     }
 }
