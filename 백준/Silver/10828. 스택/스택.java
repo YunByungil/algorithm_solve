70f2e5542ecd9afcf2c869334c88bbc95b980ec3
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-10-27 10:52:45 +0900

    [Silver IV] Title: 스택, Time: 180 ms, Memory: 17816 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -2,39 +2,43 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n; // 명령의 수
-    public static Stack<Integer> stack = new Stack<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-         n = Integer.parseInt(br.readLine());
-//        n = 1;
-
+        StringTokenizer st = null;
+        
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
+        
         for (int i = 0; i < n; i++) {
             String input = br.readLine();
-            if (input.startsWith("push")) {
-                stack.push(Integer.parseInt(input.substring(5, input.length())));
-            } else if (input.equals("pop")) {
-                if (!stack.isEmpty()) { // 비어있지 않음
-                    System.out.println(stack.pop());
-                } else { // 비어있음
-                    System.out.println(-1);
+            
+            if (input.equals("pop")) {
+                if (stack.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(stack.pop()).append("\n");
                 }
             } else if (input.equals("size")) {
-                System.out.println(stack.size());
+                sb.append(stack.size()).append("\n");
             } else if (input.equals("empty")) {
-                if (!stack.isEmpty()) { // 비어있지 않음
-                    System.out.println(0);
-                } else { // 비어있음
-                    System.out.println(1);
+                if (stack.isEmpty()) {
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
                 }
             } else if (input.equals("top")) {
-                if (!stack.isEmpty()) { // 비어있지 않음
-                    System.out.println(stack.peek());
-                } else { // 비어있음
-                    System.out.println(-1);
+                if (stack.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(stack.peek()).append("\n");
                 }
+            } else {
+                int num = Integer.parseInt(input.substring(5));
+                stack.push(num);
             }
         }
+        
+        System.out.println(sb);
     }
 }
