8ea70ed8c211a1eaa9532d35f9069f6a7f467fa1
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-10-27 15:44:00 +0900

    [Silver IV] Title: 제로, Time: 232 ms, Memory: 23940 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -2,27 +2,28 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int k;
-    public static Stack<Integer> stack = new Stack<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int result = 0;
-        k = Integer.parseInt(br.readLine());
-
+        StringTokenizer st = null;
+        
+        int k = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        
         for (int i = 0; i < k; i++) {
-            int input = Integer.parseInt(br.readLine());
-
-            if (input == 0) {
+            int num = Integer.parseInt(br.readLine());
+            
+            if (num == 0) {
                 stack.pop();
             } else {
-                stack.push(input);
+                stack.push(num);
             }
         }
-
-        for (Integer money : stack) {
-            result += money;
+        
+        int answer = 0;
+        for (int i : stack) {
+            answer += i;
         }
-
-        System.out.println(result);
+        
+        System.out.println(answer);
     }
 }
