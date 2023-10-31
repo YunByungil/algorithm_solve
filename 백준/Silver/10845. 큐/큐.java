32d06a13c0f870aca31120522c2ddb7db2beae7d
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-10-31 12:01:21 +0900

    [Silver IV] Title: 큐, Time: 172 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,57 +1,50 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static int n; // 명령의 수
-    public static LinkedList<Integer> q = new LinkedList<>();
-   
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
         
-        
+        Deque<Integer> q = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
-            
-            
             String input = br.readLine();
             
-            if (input.startsWith("push")) {
-                int num = Integer.parseInt(input.substring(5, input.length()));
-                
-                q.offer(num);
-            } else if (input.equals("pop")) {
-                if (!q.isEmpty()) {
-                    System.out.println(q.poll());
+            if (input.equals("pop")) {
+                if (q.isEmpty()) {
+                    sb.append("-1").append("\n");
                 } else {
-                    System.out.println(-1);
+                    sb.append(q.poll()).append("\n");
                 }
             } else if (input.equals("size")) {
-                System.out.println(q.size());
+                sb.append(q.size()).append("\n");
             } else if (input.equals("empty")) {
-                if (!q.isEmpty()) {
-                    System.out.println(0);
+                if (q.isEmpty()) {
+                    sb.append("1").append("\n");
                 } else {
-                    System.out.println(1);
+                    sb.append("0").append("\n");
                 }
             } else if (input.equals("front")) {
-                if (!q.isEmpty()) {
-                    int firstNum = q.pollFirst();
-                    System.out.println(firstNum);
-                    q.addFirst(firstNum);
+                if (q.isEmpty()) {
+                    sb.append("-1").append("\n");
                 } else {
-                    System.out.println(-1);
+                    sb.append(q.peekFirst()).append("\n");
                 }
             } else if (input.equals("back")) {
-                if (!q.isEmpty()) {
-                    int lastNum = q.pollLast();
-                    System.out.println(lastNum);
-                    q.addLast(lastNum);
+                if (q.isEmpty()) {
+                    sb.append("-1").append("\n");
                 } else {
-                    System.out.println(-1);
+                    sb.append(q.peekLast()).append("\n");
                 }
+            } else {
+                int num = Integer.parseInt(input.substring(5));
+                q.offer(num);
+                // sb.append(num).append("\n");
             }
         }
-               
-       
+        
+        System.out.println(sb);
     }
 }
