9f01bbe58be74908d2726f793fad18a11cb59855
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-11-02 19:23:21 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+		
+		while(true) {
+			
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if(a==0 && b==0) {
+				break;
+			}
+			sb.append((a+b)).append('\n');
+		}
+		System.out.println(sb);
+    }
+}
