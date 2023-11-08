cec740c2bd1969108c5ba684c00302d218b174ea
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-11-08 13:06:45 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 352 ms, Memory: 20452 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,51 +1,50 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = "";
+        StringTokenizer st = null;
+        
         while (true) {
             Stack<Character> stack = new Stack<>();
-            str = br.readLine();
-            if (str.equals(".")) {
+            String input = br.readLine();
+            if (input.equals(".")) {
                 break;
             }
             
-            for (int i = 0; i < str.length(); i++) {
-                char input = str.charAt(i);
-                if (input == '(' || input == '[') {
-                    stack.push(input);
-                } else if (input == ')') {
-                    if (!stack.isEmpty()) {
-                        if (stack.peek() == '(') {
+            for (int i = 0; i < input.length(); i++) {
+                char word = input.charAt(i);
+                if (word != '(' && word != ')' && word != '[' && word != ']') {
+                    continue;
+                }
+                if (stack.isEmpty()) {
+                    stack.push(word);
+                } else {
+                    if (stack.peek() == '(') {
+                        if (word == ')') {
                             stack.pop();
                         } else {
-                            stack.push(input);
+                            // System.out.println("word =" + word);
+                            stack.push(word);
                         }
-                    } else {
-                        stack.push(input);
-                    }
-                } else if (input == ']') {
-                    if (!stack.isEmpty()) {
-                        if (stack.peek() == '[') {
+                    } else if (stack.peek() == '[') {
+                        if (word == ']') {
                             stack.pop();
                         } else {
-                            stack.push(input);
+                            stack.push(word);
                         }
                     } else {
-                        stack.push(input);
+                        stack.push(word);
                     }
                 }
             }
             
-            if (stack.size() == 0) {
+            if (stack.isEmpty()) {
                 System.out.println("yes");
             } else {
                 System.out.println("no");
             }
-            
-            
         }
     }
 }
