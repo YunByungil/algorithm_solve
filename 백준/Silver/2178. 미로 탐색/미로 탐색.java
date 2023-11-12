5127cc8e40dd4e6d8c26c10da23b667463c9a04f
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-11-12 11:21:11 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14736 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,65 +1,64 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
-    public static int n, m; // 세로 n, 가로 m
-    public static int[][] arr; // 미로
-    public static int[][] dir; // 최소 거리
-    public static boolean[][] visit; // 방문 여부
-    public static int[] col = {0, 0, 1, -1};
-    public static int[] row = {1, -1, 0, 0};
+    public static int n, m;
+    public static int[] dx = {0, 0, 1, -1};
+    public static int[] dy = {-1, 1, 0, 0};
+    public static boolean[][] visit;
+    public static int[][] arr;
+    public static int[][] distance;
     public static Queue<int[]> q = new LinkedList<>();
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = null;
+    
+        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         
-        arr = new int[n + 1][m + 1];
-        visit = new boolean[n + 1][m + 1];
+        arr = new int[n][m];
+        distance = new int[n][m];
+        visit = new boolean[n][m];
         
-        dir = new int[n + 1][m + 1];
-        dir[1][1] = 1;
-        
-        for (int i = 1; i <= n; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < m; j++) {
-                arr[i][j + 1] = str.charAt(j) - '0';
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < input.length(); j++) {
+                int num = input.charAt(j) - '0';
+                arr[i][j] = num;
             }
         }
         
-        bfs(1, 1);
-        System.out.println(dir[n][m]);
+        bfs(0, 0);
+        
+        System.out.println(distance[n - 1][m - 1] + 1);
         
     }
     
-    public static void bfs(int y, int x) {
-        q.offer(new int[]{y, x});
-        visit[y][x] = true;
+    public static void bfs(int x, int y) {
+        visit[x][y] = true;
+        q.offer(new int[]{x, y});
         
         while (!q.isEmpty()) {
             int[] nowLocation = q.poll();
-            int nowY = nowLocation[0];
-            int nowX = nowLocation[1];
-            
+            int nowX = nowLocation[0];
+            int nowY = nowLocation[1];
             
             for (int i = 0; i < 4; i++) {
-                int newY = nowY + row[i];
-                int newX = nowX + col[i];
+                int newX = nowX + dx[i];
+                int newY = nowY + dy[i];
                 
-                if (newY <= 0 || newY > n || newX <= 0 || newX > m) {
+                if (newX < 0 || newX >= n || newY < 0 || newY >= m) {
                     continue;
                 }
                 
-                if (!visit[newY][newX] && arr[newY][newX] == 1) {
-                    if (dir[newY][newX] == 0) {
-                        dir[newY][newX] = dir[nowY][nowX] + 1;
-                    }                    
-                    q.offer(new int[]{newY, newX});
-                    visit[newY][newX] = true;
+                if (arr[newX][newY] == 1 && !visit[newX][newY]) {
+                    visit[newX][newY] = true;
+                    q.offer(new int[]{newX, newY});
+                    distance[newX][newY] = distance[nowX][nowY] + 1;
                 }
             }
         }
     }
-    
 }
