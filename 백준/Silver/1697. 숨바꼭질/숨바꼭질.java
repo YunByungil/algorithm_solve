b6a7399dd9dd9f4056756f1f99d3e1727caadbc5
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-11-15 10:40:54 +0900

    [Silver I] Title: 숨바꼭질, Time: 160 ms, Memory: 18628 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -2,88 +2,67 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n, k; //  n: 수빈이 위치, k: 동생 위치
-    public static int[] arr = new int[100_001]; // 좌표
-    public static boolean[] visit = new boolean[100_001];
-    public static int[] col = {1, -1, 2};
+    public static int n, k;
+    public static boolean[] visit;
+    public static int[] arr;
+    public static int[] move = {-1, 1, 2};
     public static Queue<Integer> q = new LinkedList<>();
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = null;
         
+        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
+        arr = new int[100_001];
+        visit = new boolean[100_001];
         
+        visit[n] = true;
+        q.offer(n);
+        bfs();
         
-        
-        bfs(n);
-        System.out.println(arr[k]);
-        
+        //for (int i = 0; i <= 20; i++) {
+          //  System.out.println("arr[" + i + "] = " + arr[i]);
+        //}
+         System.out.println(arr[k]);
     }
     
-    public static void bfs(int num) {
-        boolean result = true;
-        q.offer(num);
-        arr[num] = 0;
-        visit[num] = true;
-        
+    public static void bfs() {
         while (!q.isEmpty()) {
-            if (!result) {
-                break;
-            }
             int nowLocation = q.poll();
-            int move = 0;
-            for (int i = 0; i < 3; i++) {
-                if (i == 2) {
-                    move = nowLocation * col[i];    
-                } else {
-                    move = nowLocation + col[i];
-                }
-                
-                if (i == 2) {
-                    
-                    if (nowLocation > 50_000) {
-                       continue;
-                    }
-                    
-                    if (arr[move] == 0 && !visit[move]) {
-                        q.offer(move);
-                        visit[move] = true;
-                        arr[move] = arr[nowLocation] + 1;
-                    }
-                    
-                }
-                
-                else if (move < 0 || move > 100_000) {
-                    continue;
-                } else {
-                    if (arr[move] == 0 && !visit[move]) {
-                    q.offer(move);
-                    visit[move] = true;
-                    arr[move] = arr[nowLocation] + 1;
-                }
-                }
-                
-                
-                if (move == k || move == k) {
-                    result = false;
-                    break;
-                }
+            
+            int left = nowLocation + move[0];
+            int right = nowLocation + move[1];
+            int jump = nowLocation * move[2];
+            
+            // if (left < 0 || right >= 100_001 || jump >= 100_001) {
+            //     continue;
+            // }
+
+            if (left >= 0) {
+            if (!visit[left]) {
+                arr[left] = arr[nowLocation] + 1;
+                q.offer(left);
+                visit[left] = true;
+            }
+            }
+            
+            if (right < 100_001) {
+            if (!visit[right]) {
+                arr[right] = arr[nowLocation] + 1;
+                q.offer(right);
+                visit[right] = true;
+            }
+            }
+            
+            if (jump < 100_001) {
+            if (!visit[jump]) {
+                arr[jump] = arr[nowLocation] + 1;
+                q.offer(jump);
+                visit[jump] = true;
+            }
             }
         }
-    } 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    }
 }
