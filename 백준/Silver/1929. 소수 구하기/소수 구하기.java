7b8e8a3d1b8126e4209aaa4da7a791f0c0d054cc
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-11-16 11:03:30 +0900

    [Silver III] Title: 소수 구하기, Time: 904 ms, Memory: 35316 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -6,34 +6,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
-        
-        st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
-        
-        int[] arr = new int[1000001];
-        int size = arr.length - 1;
-        arr[0] = 0;
-        arr[1] = 0;
-        
+        int size = 1_000_000;
+        // int size = 20;
+        int[] arr = new int[size + 1];
         for (int i = 2; i <= size; i++) {
             arr[i] = i;
         }
         
         for (int i = 2; i <= size; i++) {
+            if (arr[i] == 0) {
+                continue;
+            }
             for (int j = i + i; j <= size; j += i) {
-                if (arr[j] == 0) {
-                    continue;
-                }
                 arr[j] = 0;
             }
         }
+        
+        st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
         
         for (int i = m; i <= n; i++) {
             if (arr[i] != 0) {
                 System.out.println(arr[i]);
             }
         }
-        
     }
 }
