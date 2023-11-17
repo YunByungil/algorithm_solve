9cd0682ce9476cbb9223ed63e98017de41c14641
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-11-17 21:46:45 +0900

    [Gold V] Title: 적록색약, Time: 168 ms, Memory: 16536 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -2,34 +2,31 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n; // 크기
-    public static int answer, answer2; // 색맹이 아닌 사람, 색맹인 사람
-    public static String color;
-    public static int[] row = {-1, 1, 0, 0};
-    public static int[] col = {0, 0, 1, -1};
-    public static String[][] arr; 
-    public static String[][] arr2;
+    public static int n, answer, answer2;
+    public static int[] dx = {0, 0, 1, -1};
+    public static int[] dy = {-1, 1, 0, 0};
     public static boolean[][] visit;
-    public static boolean[][] visit2;
+    public static char[][] arr;
+    public static char[][] arr2;
     public static Queue<int[]> q = new LinkedList<>();
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        StringTokenizer st = null;
         
-        arr = new String[n][n];
-        arr2 = new String[n][n];
+        n = Integer.parseInt(br.readLine());
         visit = new boolean[n][n];
-        visit2 = new boolean[n][n];
+        arr = new char[n][n];
+        arr2 = new char[n][n];
         
         for (int i = 0; i < n; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = String.valueOf(str.charAt(j));
-                if (arr[i][j].equals("R") || arr[i][j].equals("G")) {
-                    arr2[i][j] = "R";
-                } else {
-                    arr2[i][j] = "B";
+            String input = br.readLine();
+            for (int j = 0; j < input.length(); j++) {
+                char color = input.charAt(j);
+                arr[i][j] = color;
+                arr2[i][j] = color;
+                if (color == 'G') {
+                    arr2[i][j] = 'R';
                 }
             }
         }
@@ -37,70 +34,48 @@ public class Main {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (!visit[i][j]) {
-                    bfs(i, j, arr[i][j]);
-                    
-                }
-                
-                if (!visit2[i][j]) {
-                    bfs2(i, j, arr2[i][j]);
+                    answer++;
+                    bfs(i, j, arr);
                 }
             }
         }
         
-        System.out.print(answer + " " + answer2);
-    }
-    
-    public static void bfs(int y, int x, String c) {
-        color = c;
-
-        answer++;
-        q.offer(new int[]{y, x});
-        visit[y][x] = true;
-        
-        while (!q.isEmpty()) {
-            int[] nowLocation = q.poll();
-            int nowY = nowLocation[0];
-            int nowX = nowLocation[1];
-            
-            for (int i = 0; i < 4; i++) {
-                int newY = nowY + row[i];
-                int newX = nowX + col[i];
-                
-                if (newY < 0 || newY >= n || newX < 0 || newX >= n) {
-                    continue;
-                }
-                
-                if (!visit[newY][newX] && arr[newY][newX].equals(c)) {
-                    q.offer(new int[]{newY, newX});
-                    visit[newY][newX] = true;
+        visit = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visit[i][j]) {
+                    answer2++;
+                    bfs(i, j, arr2);
                 }
             }
         }
+        
+        System.out.println(answer + " " + answer2);
+        
+        
     }
     
-    public static void bfs2(int y, int x, String c) {
-        color = c;
-        
-        answer2++;
-        q.offer(new int[]{y, x});
-        visit[y][x] = true;
+    public static void bfs(int x, int y, char[][] arr) {
+        visit[x][y] = true;
+        q.offer(new int[]{x, y});
         
         while (!q.isEmpty()) {
             int[] nowLocation = q.poll();
-            int nowY = nowLocation[0];
-            int nowX = nowLocation[1];
+            int nowX = nowLocation[0];
+            int nowY = nowLocation[1];
+            char nowColor = arr[x][y];
             
             for (int i = 0; i < 4; i++) {
-                int newY = nowY + row[i];
-                int newX = nowX + col[i];
+                int newX = nowX + dx[i];
+                int newY = nowY + dy[i];
                 
-                if (newY < 0 || newY >= n || newX < 0 || newX >= n) {
+                if (newX < 0 || newX >= n || newY < 0 || newY >= n) {
                     continue;
                 }
                 
-                if (!visit2[newY][newX] && arr2[newY][newX].equals(c)) {
-                    q.offer(new int[]{newY, newX});
-                    visit2[newY][newX] = true;
+                if (!visit[newX][newY] && arr[newX][newY] == nowColor) {
+                    visit[newX][newY] = true;
+                    q.offer(new int[]{newX, newY});
                 }
             }
         }
