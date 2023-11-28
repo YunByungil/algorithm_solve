4c1c7fb1c083faf4eaa6e07e5780f6ca000e0396
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-11-28 18:39:40 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2288 ms, Memory: 71780 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,38 +1,35 @@
-// "static void main" must be defined in a public class.
 import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n;
-    public static Integer[][] arr;
-
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
-
-        n = Integer.parseInt(br.readLine());
-        arr = new Integer[n][2];
-
+        
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+        
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 2; j++) {
-                int num = Integer.parseInt(st.nextToken());
-                arr[i][j] = num;
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        Arrays.sort(arr, new Comparator<Integer[]>() {
+        
+        Arrays.sort(arr, new Comparator<int[]>() {
             @Override
-            public int compare(Integer[] o1, Integer[] o2) {
-                if (o1[0].equals(o2[0])) {
-                    return o1[1].compareTo(o2[1]);
-                } else {
-                    return o1[0].compareTo(o2[0]);
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
                 }
+                
+                return o1[0] - o2[0];
             }
         });
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
