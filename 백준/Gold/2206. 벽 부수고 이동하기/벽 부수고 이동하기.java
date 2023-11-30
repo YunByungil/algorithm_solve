7241fd85fcd22f93060fc2a9e6bcc4c74f9e8da2
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-11-30 17:13:03 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 900 ms, Memory: 164296 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,98 +1,82 @@
-
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static int hammer, result;
+    public static boolean state;
     public static int n, m;
-
-    public static int[][] arr;
+    public static int answer = Integer.MAX_VALUE;
+    public static int[] dx = {0, 0, -1, 1};
+    public static int[] dy = {-1, 1, 0, 0};
     public static boolean[][][] visit;
-
-    public static int[] col = {1, -1, 0, 0};
-    public static int[] row = {0, 0, 1, -1};
-
+    public static int[][] arr;
+    public static int[][] result;
     public static Queue<int[]> q = new LinkedList<>();
-
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
+        StringTokenizer st = null;
+        
+        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
-        arr = new int[n][m];
+        
         visit = new boolean[n][m][2];
-
+        arr = new int[n][m];
+        result = new int[n][m];
+        
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            String str = st.nextToken();
+            String input = br.readLine();
             for (int j = 0; j < m; j++) {
-                arr[i][j] = str.charAt(j) - '0';
+                int num = input.charAt(j) - '0';
+                arr[i][j] = num;
             }
         }
-
-        bfs();
-
-//        result = arr[n - 1][m - 1];
-
-
+        
+        answer = bfs();
+        
         if (n - 1 == 0 && m - 1 == 0) {
             System.out.println(1);
         } else {
-            System.out.println(result);
+            System.out.println(answer);
         }
-
-
-
     }
-
-    public static void bfs() {
-        arr[0][0] = 1;
-        visit[0][0][0] = true;
-        visit[0][0][1] = true;
+    
+    public static int bfs() {
         q.offer(new int[]{0, 0, 0});
-
         while (!q.isEmpty()) {
             int[] nowLocation = q.poll();
-            int y = nowLocation[0];
-            int x = nowLocation[1];
+            int nowX = nowLocation[0];
+            int nowY = nowLocation[1];
             int crashed = nowLocation[2];
-
+            
             for (int i = 0; i < 4; i++) {
-                int newY = y + row[i];
-                int newX = x + col[i];
-
-                if (newY < 0 || newX < 0 || newY >= n || newX >= m) {
+                int newX = nowX + dx[i];
+                int newY = nowY + dy[i];
+                
+                if (newX < 0 || newX >= n || newY < 0 || newY >= m) {
                     continue;
                 }
-
-
-                if (arr[newY][newX] == 1) {
-                    if (crashed == 0 && !visit[newY][newX][1]) {
-                        visit[newY][newX][crashed] = true;
-                        arr[newY][newX] = arr[y][x] + 1;
-                        q.offer(new int[]{newY, newX, 1});
+                
+                if (arr[newX][newY] == 1) {
+                    if (crashed == 0 && !visit[newX][newY][1]) {
+                        visit[newX][newY][crashed] = true;
+                        result[newX][newY] = result[nowX][nowY] + 1;
+                        q.offer(new int[]{newX, newY, 1});
                     }
                 } else {
-                    if (!visit[newY][newX][crashed]) {
-                        visit[newY][newX][crashed] = true;
-                        arr[newY][newX] = arr[y][x] + 1;
-                        q.offer(new int[]{newY, newX, crashed});
+                    if (!visit[newX][newY][crashed]) {
+                        visit[newX][newY][crashed] = true;
+                        result[newX][newY] = result[nowX][nowY] + 1;
+                        q.offer(new int[]{newX, newY, crashed});
                     }
                 }
-
-                if (newY == n - 1 && newX == m - 1) {
-                    result = arr[n - 1][m - 1];
-                    return;
+                
+                if (newX == n - 1 && newY == m - 1) {
+                    return result[n - 1][m - 1] + 1;
                 }
             }
         }
-        result = -1;
+        
+        return -1;
     }
 }
