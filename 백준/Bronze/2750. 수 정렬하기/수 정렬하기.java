4341175be31e6dad99a20f512c357b061cdc3273
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-12-17 23:52:59 +0900

    [Bronze II] Title: 수 정렬하기, Time: 172 ms, Memory: 14884 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,23 +1,24 @@
-// "static void main" must be defined in a public class.
 import java.io.*;
 import java.util.*;
-
+ 
 public class Main {
-    public static int n;
-    public static int[] arr;
+	public static void main(String[] args) throws IOException {
     
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        
-        arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(arr);
-        
-        for (int i : arr) {
-            System.out.println(i);
-        }
-    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+ 
+		// 정렬 메소드
+		Arrays.sort(arr);
+		
+		for(int i : arr) {
+			System.out.println(i);
+		}
+ 
+	}
 }
