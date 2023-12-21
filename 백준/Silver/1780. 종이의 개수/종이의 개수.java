33cacef6fddac66d22f829ada3d7ea01bfaea236
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-12-21 15:38:16 +0900

    [Silver II] Title: 종이의 개수, Time: 1072 ms, Memory: 313692 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -1,67 +1,68 @@
-// "static void main" must be defined in a public class.
 import java.util.*;
 import java.io.*;
 
 public class Main {
     public static int n;
+    public static int result1, result2, result3;
     public static int[][] arr;
-    public static int minus, zero, one;
-    
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n][n];
         StringTokenizer st = null;
+
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+        
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                int num = Integer.parseInt(st.nextToken());
+                arr[i][j] = num;
             }
         }
-        dfs(0, 0, n);
-        System.out.println(minus);
-        System.out.println(zero);
-        System.out.println(one);
+        
+        
+        dfs(n, 0, 0);
+        System.out.println(result1 + "\n" + result2 + "\n" + result3);
         
     }
     
-    public static void dfs(int y, int x, int n) {
-        if (check(y, x, n)) {
-            if (arr[y][x] == 0) {
-                zero++;
-            } else if (arr[y][x] == 1) {
-                one++;
-            } else if (arr[y][x] == -1) {
-                minus++;
+    public static void dfs(int n, int x, int y) {
+        if (check(n, x, y)) {
+            int result = arr[x][y];
+            if (result == -1) {
+                result1++;
+            } else if (result == 0) {
+                result2++;
+            } else {
+                result3++;
             }
             
             return;
         }
         
-        int half = n / 3;
-        dfs(y, x, half);
-        dfs(y, x + half, half);
-        dfs(y, x + half + half, half);
+        int size = n / 3;
         
-        dfs(y + half, x, half);
-        dfs(y + half, x + half, half);
-        dfs(y + half, x + half + half, half);
+        dfs(size, x, y);
+        dfs(size, x, y + size);
+        dfs(size, x, y + 2 * size);
         
-        dfs(y + half + half, x, half);
-        dfs(y + half + half, x + half, half);
-        dfs(y + half + half, x + half + half, half);
+        dfs(size, x + size, y);
+        dfs(size, x + size, y + size);
+        dfs(size, x + size, y + 2 * size);
         
-        
-        
+        dfs(size, x + 2 * size, y);
+        dfs(size, x + 2 * size, y + size);
+        dfs(size, x + 2 * size, y + 2 * size);
     }
     
-    public static boolean check(int y, int x, int size) {
-        int color = arr[y][x];
+    
+    public static boolean check(int size, int x, int y) {
+        int now = arr[x][y];
         
-        for (int i = y; i < y + size; i++) {
-            for (int j = x; j < x + size; j++) {
-                if (arr[i][j] != color) {
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                if (arr[i][j] != now) {
                     return false;
                 }
             }
@@ -69,5 +70,4 @@ public class Main {
         
         return true;
     }
-    
 }
