635ee88071aa74dd206b91e81b55903000f7d7a2
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-12-27 19:38:31 +0900

    [Silver I] Title: 곱셈, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -2,36 +2,35 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    // public static long a, b, c;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
-        
+
         st = new StringTokenizer(br.readLine());
-        long a = Integer.parseInt(st.nextToken());
-        long b = Integer.parseInt(st.nextToken());
-        long c = Integer.parseInt(st.nextToken());
-        
-        long answer = pow(a, b, c);
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
         
         
-        System.out.println(answer);
+        System.out.println(dfs(a, b, c));
     }
     
-    public static long pow(long a, long b, long c) {
+    public static long dfs(long a, long b, long c) {
         if (b == 1) {
             return a % c;
         }
         
-        long value = pow(a, b / 2, c);
+        long value = dfs(a, b / 2, c);
         value = value * value % c;
         
         if (b % 2 == 0) {
             return value;
+        } else {
+            return value * a % c;
         }
         
-        return value * a % c;
+        
+        
     }
-    
 }
