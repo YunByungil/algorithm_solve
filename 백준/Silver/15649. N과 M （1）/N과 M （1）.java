693570e58c35c78e776afe11929c5caf42a17586
윤병일 <109706328+YunByungil@users.noreply.github.com>
2023-12-28 13:42:44 +0900

    [Silver III] Title: N과 M (1), Time: 2392 ms, Memory: 75496 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,4 +1,3 @@
-// "static void main" must be defined in a public class.
 import java.util.*;
 import java.io.*;
 
@@ -6,42 +5,37 @@ public class Main {
     public static int n, m;
     public static int[] arr;
     public static boolean[] visit;
-    public static StringBuilder sb = new StringBuilder();
-        
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        n = Integer.parseInt(st.nextToken()); // 1부터 n까지 자연수 중
-        m = Integer.parseInt(st.nextToken()); // 중복 없이 m개를 고른 수열
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         
         arr = new int[n];
         visit = new boolean[n];
         
         dfs(0);
-        System.out.println(sb);
     }
     
     public static void dfs(int depth) {
         if (depth == m) {
             for (int i = 0; i < m; i++) {
-                sb.append(arr[i]).append(" ");
-                
+                System.out.print(arr[i] + " ");
             }
-            sb.append("\n");
+            System.out.println();
             return;
         }
         
         for (int i = 0; i < n; i++) {
-            
             if (!visit[i]) {
                 visit[i] = true;
                 arr[depth] = i + 1;
-                // System.out.println("gg = " + (i + 1));
                 dfs(depth + 1);
                 visit[i] = false;
             }
-            
         }
     }
 }
