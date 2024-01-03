e6d97779adf7a385a2e789def877b317727f467a
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-01-03 12:25:41 +0900

    [Silver III] Title: N과 M (6), Time: 140 ms, Memory: 15800 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,39 +1,42 @@
-// "static void main" must be defined in a public class.
 import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n, m; // n 개의 자연수 중 m개를 고른 수열
-    public static int[] arr;
-    public static int[] numArr;
+    public static int n, m;
+    public static int[] arr, temp;
     public static boolean[] visit;
     public static StringBuilder sb = new StringBuilder();
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
         
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         
         arr = new int[m];
-        numArr = new int[n];
+        temp = new int[n];
         visit = new boolean[n];
+        
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            numArr[i] = Integer.parseInt(st.nextToken());
+            temp[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(numArr);
-        dfs(0, 0);
+        Arrays.sort(temp);
         
+        dfs(0, 0);
         System.out.println(sb);
     }
     
     public static void dfs(int depth, int start) {
         if (depth == m) {
-            for (int i = 0 ; i < m; i++) {
-                sb.append(arr[i]).append(" ");
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i] + " ");
+                // System.out.print(arr[i] + " ");
             }
+            // System.out.println();
             sb.append("\n");
             
             return;
@@ -42,7 +45,7 @@ public class Main {
         for (int i = start; i < n; i++) {
             if (!visit[i]) {
                 visit[i] = true;
-                arr[depth] = numArr[i];
+                arr[depth] = temp[i];
                 dfs(depth + 1, i);
                 visit[i] = false;
             }
