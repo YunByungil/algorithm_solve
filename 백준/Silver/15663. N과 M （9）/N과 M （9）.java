8b95cd2be31d0ff7441f168f010dfe7e8e8cd355
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-01-03 15:47:44 +0900

    [Silver II] Title: N과 M (9), Time: 712 ms, Memory: 52360 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,51 +1,62 @@
 import java.util.*;
 import java.io.*;
 
-class Main {
-    static int N, M, check;
-    static int[] arr;
-    static int[] numArr;
-    static boolean[] visit;
-
-    static StringBuilder sb = new StringBuilder();
-
+public class Main {
+    public static int n, m;
+    public static Set<String> set = new LinkedHashSet<>();
+    public static int[] arr, temp;
+    public static boolean[] visit;
+    public static StringBuilder sb = new StringBuilder();
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new int[M];
-        visit = new boolean[N];
-        numArr = new int[N];
+        StringTokenizer st = null;
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            numArr[i] = Integer.parseInt(st.nextToken());
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        arr = new int[m];
+        temp = new int[n];
+        visit = new boolean[n];
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            temp[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(numArr);
+        Arrays.sort(temp);
+        
         dfs(0);
-        System.out.println(sb);
+        List<String> list = new ArrayList<>(set);
+        
+        for (String str : list) {
+            System.out.print(str);
+        }
     }
-
-    private static void dfs(int depth) {
-        if (depth == M) { //원하는 만큼 뽑았으면 탈출
-            for (int i = 0; i < M; i++) {
-                sb.append(arr[i]).append(" ");
-
+    
+    public static void dfs(int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i] + " ");
+                // System.out.print(arr[i] + " ");
             }
+            // System.out.println();
             sb.append("\n");
+            
+            set.add(sb.toString());
+            // System.out.println("result = " + result);
+            sb = new StringBuilder();
             return;
         }
-            for (int i = 0; i < N; i++) {
-                if (!visit[i] && arr[depth] != numArr[i] ) {
-                    visit[i] = true;
-                    arr[depth] = numArr[i];
-//                    System.out.println("check = " + check);
-                    dfs(depth + 1);
-                    visit[i] = false;
-                }
+        
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = temp[i];
+                dfs(depth + 1);
+                visit[i] = false;
             }
-            arr[depth] = 0;
         }
+    }
 }
