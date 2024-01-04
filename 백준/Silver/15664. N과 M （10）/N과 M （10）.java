d5c859da6ef1e0b97241f9fedc22b0cfa16b8dbe
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-01-04 13:10:12 +0900

    [Silver II] Title: N과 M (10), Time: 144 ms, Memory: 15836 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.java
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -1,54 +1,55 @@
-// "static void main" must be defined in a public class.
 import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n, m; // n 개의 자연수 중 m개를 고른 수열
-    public static int check;
-    public static int[] arr;
-    public static int[] numArr;
+    public static int n, m;
+    public static Set<String> set = new LinkedHashSet<>();
+    public static int[] arr, temp;
     public static boolean[] visit;
     public static StringBuilder sb = new StringBuilder();
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
         
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         
         arr = new int[m];
-        numArr = new int[n];
+        temp = new int[n];
         visit = new boolean[n];
+        
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            numArr[i] = Integer.parseInt(st.nextToken());
+            temp[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(numArr);
-        dfs(0, 0);
+        Arrays.sort(temp);
         
+        dfs(0, 0);
         System.out.println(sb);
     }
     
     public static void dfs(int depth, int start) {
         if (depth == m) {
-            for (int i = 0 ; i < m; i++) {
-                sb.append(arr[i]).append(" ");
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i] + " ");
             }
             sb.append("\n");
             
             return;
         }
-        int check = 0;
+        
+        int tmp = 0;
         for (int i = start; i < n; i++) {
-            if (!visit[i] && check != numArr[i]) {
+            if (!visit[i] && tmp != temp[i]) {
                 visit[i] = true;
-                arr[depth] = numArr[i];
-                check = numArr[i];
-                dfs(depth + 1, i);
+                arr[depth] = temp[i];
+                tmp = arr[depth];
+                dfs(depth + 1, i + 1);
                 visit[i] = false;
             }
         }
-        
     }
 }
