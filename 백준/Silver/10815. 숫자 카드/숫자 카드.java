2b8743d0173beafb20e740012b097bc4570e4a31
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-01-10 23:32:52 +0900

    [Silver V] Title: 숫자 카드, Time: 3608 ms, Memory: 217592 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,47 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // 1. input 받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        Map<Integer, Integer> card = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            int base = Integer.parseInt(st.nextToken());
+            card.put(base, 1);
         }
-        Arrays.sort(arr);
-
         st = new StringTokenizer(br.readLine());
         int m = Integer.parseInt(st.nextToken());
+        Map<Integer, Integer> result = new LinkedHashMap<>();
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            sb.append(binarySearch(arr, 0, n -1, Integer.parseInt(st.nextToken()))).append(" ");
+            int input = Integer.parseInt(st.nextToken());
+            result.put(input, 0);
         }
 
-        System.out.println(sb);
-
-    }
-
-    public static int binarySearch(int[] arr, int start, int end, int target) {
-        int mid = (start + end) / 2;
-        while (start <= end) {
-
-            if (arr[mid] == target) {
-                return 1;
-            } else if (arr[mid] <= target) {
-                start = mid + 1;
-                mid = (start + end) / 2;
-            } else {
-                end = mid - 1;
-                mid = (start + end) / 2;
+        for (Integer integer : card.keySet()) {
+            if (result.containsKey(integer)) {
+                result.put(integer, 1);
             }
         }
-        return 0;
+
+        for (Integer integer : result.keySet()) {
+            System.out.print(result.get(integer) + " ");
+        }
     }
 }
