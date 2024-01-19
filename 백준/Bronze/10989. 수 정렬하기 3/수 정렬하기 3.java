56b4dff9c158d8666b1d33427a969fb6f8cb6d81
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-01-19 18:01:10 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1704 ms, Memory: 337168 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,27 +1,31 @@
-// "static void main" must be defined in a public class.
+import java.util.*;
 import java.io.*;
 
 public class Main {
-        
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
         StringBuilder sb = new StringBuilder();
+        
         int n = Integer.parseInt(br.readLine());
         
-        int[] arr = new int[n];
-        int[] count = new int[10000001];
-        
+        int[] arr = new int[10_001];
         for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(br.readLine());
-            count[num]++;
+            arr[num]++;
         }
         
-        for (int i = 0; i < count.length; i++) {
-            while (count[i] > 0) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] <= 0) continue;
+            while (arr[i] > 0) {
                 sb.append(i).append("\n");
-                count[i]--;
+                arr[i]--;
             }
         }
+        
         System.out.println(sb);
     }
 }
+    
+    
