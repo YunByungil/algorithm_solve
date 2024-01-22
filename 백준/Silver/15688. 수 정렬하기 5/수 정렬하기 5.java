b15a75aa3d5e6955189a3b6674282fa9aed307e4
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-01-22 10:26:02 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 19076 ms, Memory: 102952 KB -BaekjoonHub

--- 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -1,28 +1,34 @@
-// "static void main" must be defined in a public class.
+import java.util.*;
 import java.io.*;
 
 public class Main {
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        
         StringBuilder sb = new StringBuilder();
         
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        int[] count = new int[20000001];
+        int[] arr = new int[2_000_001];
         
         for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(br.readLine());
-            count[num + 1000000]++;
+            arr[num + 1_000_000]++;
         }
         
-        for (int i = 0; i < count.length; i++) {
-            while (count[i] > 0) {
-                sb.append(i - 1000000).append("\n");
-                count[i]--;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= 0) continue;
+            while (arr[i] > 0) {
+                // System.out.println(i - 1_000_000);
+                sb.append(i - 1_000_000).append("\n");
+                arr[i]--;
             }
         }
         
         System.out.println(sb);
+        
     }
 }
+    
+    
