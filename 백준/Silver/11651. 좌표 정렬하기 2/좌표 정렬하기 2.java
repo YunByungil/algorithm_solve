d49b42f1f8882bfd1b80152131f3502595636138
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-01-23 13:50:57 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 2408 ms, Memory: 76736 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -1,43 +1,39 @@
-// "static void main" must be defined in a public class.
 import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n;
-    public static Integer[][] arr;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
         
-        n = Integer.parseInt(br.readLine());
-        arr = new Integer[n][2];
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
         
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 2; j++) {
-                int num = Integer.parseInt(st.nextToken());
-                arr[i][j] = num;
-            }
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arr[i][0] = x;
+            arr[i][1] = y;
         }
         
-        Arrays.sort(arr, new Comparator<Integer[]>() {
-           @Override
-            public int compare(Integer[] o1, Integer[] o2) {
-                if (o1[1].equals(o2[1])) {
-                    return o1[0].compareTo(o2[0]);
-                } else {
-                    return o1[1].compareTo(o2[1]);
-                }
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
             }
+            return o1[1] - o2[1];
         });
         
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
         
     }
+    
 }
+    
+    
