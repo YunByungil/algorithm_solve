72ae21ecd698bed3c9e8f2e234afc1d370f596d0
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-01-30 14:22:23 +0900

    [Silver IV] Title: 카드, Time: 388 ms, Memory: 36660 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -1,38 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+    public static int n;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int maxCount = 0;
-        long maxValue = Long.MIN_VALUE;
-        int count = 0;
+        StringTokenizer st = null;
+
+        n = Integer.parseInt(br.readLine());
         long[] arr = new long[n];
+
         for (int i = 0; i < n; i++) {
             arr[i] = Long.parseLong(br.readLine());
         }
         Arrays.sort(arr);
 
-        for (int i = 0; i < n; i++) {
-            if (i == 0 || arr[i - 1] == arr[i]) {
-                count++;
-            } else {
-                if (count > maxCount) {
-                    maxValue = arr[i - 1];
-                    maxCount = count;
-                }
+        Map<Long, Long> map = new HashMap<>();
+        for (long i : arr) {
+            map.put(i, map.getOrDefault(i, 0L) + 1);
+        }
 
-                count = 1;
+        long first = 0;
+        long num = 0;
+        long count = 0;
+        for (Long i : map.keySet()) {
+            if (first == 0) {
+                first = -1;
+                count = map.get(i);
+                num = i;
+                continue;
+            }
+            if (count < map.get(i)) {
+                count = map.get(i);
+                num = i;
+            } else if (count == map.get(i)) {
+                num = Math.min(num, i);
             }
         }
 
-        if (count > maxCount) {
-            maxValue = arr[n - 1];
-        }
-
-        System.out.println(maxValue);
+        System.out.println(num);
     }
 }
