6ee3d8b30268bc7c6730dfb3f475db4d928ba65c
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-02-02 17:12:08 +0900

    [Silver IV] Title: 카드, Time: 300 ms, Memory: 25428 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -2,43 +2,47 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n;
-
+    public static int n, c;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
-
+        
+        // st = new StringTokenizer(br.readLine());
+        // n = Integer.parseInt(st.nextToken());
+        // c = Integer.praseInt(st.nextToken());
         n = Integer.parseInt(br.readLine());
         long[] arr = new long[n];
-
         for (int i = 0; i < n; i++) {
             arr[i] = Long.parseLong(br.readLine());
         }
+        
         Arrays.sort(arr);
-
-        Map<Long, Long> map = new HashMap<>();
-        for (long i : arr) {
-            map.put(i, map.getOrDefault(i, 0L) + 1);
-        }
-
-        long first = 0;
-        long num = 0;
-        long count = 0;
-        for (Long i : map.keySet()) {
-            if (first == 0) {
-                first = -1;
-                count = map.get(i);
-                num = i;
+        
+        int count = 0;
+        int maxCount = 0;
+        long answer = 0;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || arr[i - 1] == arr[i]) {
+                count++;
                 continue;
-            }
-            if (count < map.get(i)) {
-                count = map.get(i);
-                num = i;
-            } else if (count == map.get(i)) {
-                num = Math.min(num, i);
+            } else {
+                if (count > maxCount) {
+                    answer = arr[i - 1];
+                    maxCount = count;
+                }
+                count = 1;
             }
         }
-
-        System.out.println(num);
+        
+        if (count > maxCount) {
+            answer = arr[n - 1];
+        }
+        
+        System.out.println(answer);
+        
+        
     }
 }
+    
+    
