a338d28014c01988ae6a9a066d93fb6defd78a5f
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-02-09 18:17:26 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 18132 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,28 +2,37 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-   
+    public static int n;
+    public static int[] arr;
+    
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n + 1];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
+        StringTokenizer st = null;
         
-        dp[1] = 0;
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+        // arr[1] = 1;
         
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + 1;
             
             if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
+                arr[i] = Math.min(arr[i - 1] + 1, arr[i / 3] + 1);
+            } else if (i % 2 == 0) {
+                arr[i] = Math.min(arr[i - 1] + 1, arr[i / 2] + 1);
+            } else {
+                arr[i] = arr[i - 1] + 1;
             }
             
-            if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
+            if (i % 6 == 0) {
+                arr[i] = Math.min(Math.min(arr[i / 2], arr[i / 3]), arr[i - 1]) + 1;
             }
-            
         }
-
-        System.out.println(dp[n]);
-    }
+        
+        // for (int i = 1; i <= n; i++) {
+        //     System.out.println("arr[" + i + "] = " + arr[i]);
+        // }
+        System.out.println(arr[n]);
+	}
 }
+    
+    
