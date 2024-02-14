2cd43af2c00581612c208d23e988cee1cdb91e0d
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-02-14 15:13:27 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 18064 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,35 +2,27 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n;
-    public static int[] arr;
+    public static int n, t;
+    public static int[] arr, dp;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
         StringTokenizer st = null;
         
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
         arr = new int[n + 1];
-        // arr[1] = 1;
         
         for (int i = 2; i <= n; i++) {
-            
+            arr[i] = arr[i - 1] + 1;
             if (i % 3 == 0) {
-                arr[i] = Math.min(arr[i - 1] + 1, arr[i / 3] + 1);
-            } else if (i % 2 == 0) {
-                arr[i] = Math.min(arr[i - 1] + 1, arr[i / 2] + 1);
-            } else {
-                arr[i] = arr[i - 1] + 1;
-            }
+                arr[i] = Math.min(arr[i / 3] + 1, arr[i]);
+            } 
             
-            if (i % 6 == 0) {
-                arr[i] = Math.min(Math.min(arr[i / 2], arr[i / 3]), arr[i - 1]) + 1;
+            if (i % 2 == 0) {
+                arr[i] = Math.min(arr[i / 2] + 1, arr[i]);
             }
         }
         
-        // for (int i = 1; i <= n; i++) {
-        //     System.out.println("arr[" + i + "] = " + arr[i]);
-        // }
         System.out.println(arr[n]);
 	}
 }
