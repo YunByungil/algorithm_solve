7866f2dee87c43a3511ea830b646124290dbc443
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-02-22 15:05:21 +0900

    [Silver I] Title: 1로 만들기 2, Time: 172 ms, Memory: 23620 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -2,38 +2,46 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static int n, m;
+    public static int[] arr, red, green, blue;
+    public static int[] dp;
     
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n + 1];
-        int[] arr = new int[n + 1];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
+        StringTokenizer st = null;
         
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+        dp = new int[n + 1];
+        
+        dp[1] = arr[1];
         for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + 1;
             arr[i] = i - 1;
             
-            if (i % 2 == 0) {
-                if (dp[i / 2] + 1 < dp[i]) {
-                    dp[i] = dp[i / 2] + 1;
-                    arr[i] = i / 2;
-                }
+            if (i % 2 == 0 && dp[i / 2] + 1 <= dp[i]) {
+                dp[i] = dp[i / 2] + 1;
+                // dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+                arr[i] = i / 2;
             }
             
-            if (i % 3 == 0) {
-                if (dp[i / 3] + 1 < dp[i]) {
-                    dp[i] = dp[i / 3] + 1;
-                    arr[i] = i / 3;
-                }
+            if (i % 3 == 0 && dp[i / 3] + 1 <= dp[i]) {
+                dp[i] = dp[i / 3] + 1;
+                arr[i] = i / 3;
             }
         }
         
         System.out.println(dp[n]);
-        
-        while (n > 0) {
-            System.out.print(n + " ");
+        System.out.print(n + " ");
+        // System.out.println("gd = " + arr[n]);
+        while (true) {
+            if (arr[n] == 0) {
+                break;
+            }
+            System.out.print(arr[n] + " ");
             n = arr[n];
         }
-        
-    }
+	}
 }
+    
+    
