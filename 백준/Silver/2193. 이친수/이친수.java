2bb59ef63774114f88dc7993456be5bb32f93b67
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-02-27 02:41:30 +0900

    [Silver III] Title: 이친수, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -2,19 +2,28 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static int n;
+    public static int[][] arr;
+    public static long[] dp;
     
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
         StringTokenizer st = null;
-        int n = Integer.parseInt(br.readLine());
-        long[] dp = new long[n + 1];
-        dp[1] = 1;
-        // dp[2] = 1;
         
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        dp = new long[91];
+        
+        dp[1] = 1;
+        dp[2] = 1;
+        
+        for (int i = 3; i <= 90; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
         }
         
         System.out.println(dp[n]);
     }
+    
 }
+    
+    
