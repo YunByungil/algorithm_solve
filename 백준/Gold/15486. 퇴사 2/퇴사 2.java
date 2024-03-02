17c86bf7d2f0a0fe1fbf31888372bb81866b6213
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-03-02 12:37:25 +0900

    [Gold V] Title: 퇴사 2, Time: 724 ms, Memory: 321880 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -2,32 +2,49 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static int n;
+    public static int[] arr, t, p;
+    public static int[] dp;
     
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
+        StringTokenizer st = null;
         
-        int n = Integer.parseInt(br.readLine());
-        int[] t = new int[n + 1];
-        int[] p = new int[n + 1];
-        int[] dp = new int[n + 2];
-        
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+        t = new int[n + 1];
+        p = new int[n + 1];
+        dp = new int[n + 2];
         
         for (int i = 1; i <= n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            t[i] = Integer.parseInt(st.nextToken());
-            p[i] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            int numT = Integer.parseInt(st.nextToken());
+            int numP = Integer.parseInt(st.nextToken());
+            t[i] = numT;
+            p[i] = numP;
         }
         
+        int answer = 0;
         for (int i = 1; i <= n; i++) {
-            if (i + t[i] <= n + 1) {
-                dp[i + t[i]] = Math.max(dp[i + t[i]], p[i] + dp[i]);
-            }
+            dp[i] = Math.max(dp[i], dp[i - 1]);
+            // System.out.println("dp[" + i + "] = " + dp[i]);
             
-            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+            if (i + t[i] - 1 <= n) {
+                // System.out.println("i + t[i] = " + (i + t[i]));
+                // System.out.println("dp[i] + p[i] = " + (dp[i] + p[i]));
+                dp[i + t[i]] = Math.max(dp[i + t[i]], dp[i] + p[i]);
+            }
+            // break;
         }
         
-
-        System.out.println(Math.max(dp[n], dp[n + 1]));
+        
+        for (int i = 1; i <= n + 1; i++) {
+            answer = Math.max(answer, dp[i]);
+            // System.out.println(dp[i]);
+        }
+        System.out.println(answer);
         
     }
 }
+    
+    
