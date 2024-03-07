90ebd19b34bc7817366ef8290fb8e4288bba5086
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-03-07 22:15:56 +0900

    [Silver IV] Title: 동전 0, Time: 128 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -2,14 +2,19 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-   
+    public static int n, k;
+    public static int[] arr, t, p;
+    public static long[]dp;
+    
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
+        StringTokenizer st = null;
+        st = new StringTokenizer(br.readLine());
         
-        int[] arr = new int[n];
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
@@ -17,10 +22,11 @@ public class Main {
         int answer = 0;
         for (int i = n - 1; i >= 0; i--) {
             answer += k / arr[i];
-            k = k % arr[i];
+            k %= arr[i];
         }
         
         System.out.println(answer);
-        
     }
 }
+    
+    
