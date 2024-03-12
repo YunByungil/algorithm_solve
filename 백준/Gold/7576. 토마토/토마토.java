c09052c893ca559f239b6c83db0ff38e470e93bb
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-03-12 21:09:41 +0900

    [Gold V] Title: 토마토, Time: 692 ms, Memory: 125548 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,50 +2,56 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n, m, answer;
-    public static int[] dx = {0, 0, -1, 1};
-    public static int[] dy = {-1, 1, 0, 0};
+    public static int n, m;
+    public static int[] dx = {-1, 1, 0, 0};
+    public static int[] dy = {0, 0, -1, 1};
     public static boolean[][] visit;
-    public static int[][] arr;
+    public static int[][] arr, result;
     public static Queue<int[]> q = new LinkedList<>();
     
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
         
         st = new StringTokenizer(br.readLine());
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
-        
         arr = new int[n][m];
+        result = new int[n][m];
         visit = new boolean[n][m];
         
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                int num = Integer.parseInt(st.nextToken());
-                if (num == 1) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if (arr[i][j] == 1) {
+                    result[i][j] = 1;
+                    visit[i][j] = true;
                     q.offer(new int[]{i, j});
                 }
-                arr[i][j] = num;
+                if (arr[i][j] == -1) {
+                    visit[i][j] = true;
+                    result[i][j] = -1;
+                }
             }
         }
         
         bfs();
-        
+        int answer = -1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (arr[i][j] == 0) {
+                if (result[i][j] == 0) {
                     System.out.println(-1);
                     return;
                 }
+                // System.out.print(result[i][j] + " ");
                 
-                answer = Math.max(answer, arr[i][j]);
+                answer = Math.max(answer, result[i][j]);
             }
+            // System.out.println();
         }
         
         System.out.println(answer - 1);
-        
     }
     
     public static void bfs() {
@@ -53,6 +59,7 @@ public class Main {
             int[] nowLocation = q.poll();
             int nowX = nowLocation[0];
             int nowY = nowLocation[1];
+            // visit[nowX][nowY] = true;
             
             for (int i = 0; i < 4; i++) {
                 int newX = nowX + dx[i];
@@ -62,12 +69,13 @@ public class Main {
                     continue;
                 }
                 
-                if (!visit[newX][newY] && arr[newX][newY] == 0) {
-                    visit[newX][newY] = true;
-                    arr[newX][newY] = arr[nowX][nowY] + 1;
+                if (arr[newX][newY] != -1 && !visit[newX][newY]) {
                     q.offer(new int[]{newX, newY});
+                    result[newX][newY] = result[nowX][nowY] + 1;
+                    visit[newX][newY] = true;
                 }
             }
+            
         }
     }
 }
