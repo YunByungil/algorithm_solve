21542c6697627171d2f8c5fc408e295d45716969
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-03-26 18:19:19 +0900

    [Bronze V] Title: 웰컴, Time: 116 ms, Memory: 13992 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n, m, t, k;
+    public static int[] arr, tmp;
+    public static int[] dp;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
+        StringTokenizer st = null;
+        
+        System.out.print(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");        
+    }
+}
+    
+    
