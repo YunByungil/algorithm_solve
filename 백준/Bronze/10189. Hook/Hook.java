f6862ce04c68fd3fc0159aae580984cc7e51b871
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-03-27 23:17:19 +0900

    [Bronze V] Title: Hook, Time: 120 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n, m, t, k;
+    public static int[] arr, tmp;
+    public static int[] dp;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
+        StringTokenizer st = null;
+        
+        System.out.println("#  # #### #### #  #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#  # #### #### #  #");  
+    }
+}
+    
+    
