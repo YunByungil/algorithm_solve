d9ca00fe79e31ed9b24db4443f78fff6efeb1d41
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-03-30 23:53:42 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 120 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n, m, t, k;
+    public static int[] arr, tmp;
+    public static boolean[][] dp;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
+        StringTokenizer st = null;
+        
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.print("'-..-'|_.-''-._|");
+        
+    }
+}
+    
+    
