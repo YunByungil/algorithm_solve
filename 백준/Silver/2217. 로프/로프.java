914f8dab8bcb4bce699a47a12d82023eb2cdda11
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-04-04 23:11:20 +0900

    [Silver IV] Title: 로프, Time: 276 ms, Memory: 25024 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -2,24 +2,31 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static int n, m, k, t, w;
+    public static int[] arr, tmp;
+    public static int[] dp;
     
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
         StringTokenizer st = null;
         
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(arr);
         
-        int max = 0;
-        for (int i = 1; i <= n; i++) {
-            max = Math.max(max, i * arr[n - i]);
+        Arrays.sort(arr);
+        int answer = arr[n - 1];
+        int count = 0;
+        for (int i = n; i > 0; i--) {
+            // System.out.println(arr[count++] * i);
+            answer = Math.max(answer, arr[count++] * i);
         }
         
-        System.out.println(max);
+        System.out.println(answer);
+        
     }
 }
+    
+    
