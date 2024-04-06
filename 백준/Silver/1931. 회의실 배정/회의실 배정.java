b12898a7911fa429a705e37de5e9db4724336be0
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-04-06 18:25:25 +0900

    [Silver I] Title: 회의실 배정, Time: 552 ms, Memory: 43464 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,41 +1,49 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-   
+    public static int n, m, k, t, w;
+    public static int[][] arr, tmp;
+    public static int[] dp;
+    public static int[] a, b;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
 
-        int n = Integer.parseInt(br.readLine());
-        
-        int[][] arr = new int[n][2];
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][2];
+
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 2; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                arr[i][j] = num;
+            }
         }
-        
+
         Arrays.sort(arr, new Comparator<int[]>() {
             @Override
-            public int compare(int[] o1, int[] o2) {
+            public int compare(final int[] o1, final int[] o2) {
                 if (o1[1] == o2[1]) {
                     return o1[0] - o2[0];
                 }
-                
+
                 return o1[1] - o2[1];
             }
         });
-        
-        int now = arr[0][1];
-        int answer = 1;
 
+        int start = arr[0][1];
+        int answer = 1;
         for (int i = 1; i < n; i++) {
-            if (now <= arr[i][0]) {
-                now = arr[i][1];
+            if (start <= arr[i][0]) {
+//                System.out.println("start = " + start);
                 answer++;
+                start = arr[i][1];
             }
         }
-        
+
         System.out.println(answer);
+
     }
 }
