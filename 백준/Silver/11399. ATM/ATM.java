a8a5097d3092ebf5c48eb70e124b68c2dd9ce3da
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-04-08 23:15:13 +0900

    [Silver IV] Title: ATM, Time: 140 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -2,28 +2,36 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static int n, m, k, t, w;
+    public static int[] arr, tmp;
+    public static int[] dp;
     
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
         StringTokenizer st = null;
         
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
         
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        
         Arrays.sort(arr);
         
-        int result = arr[0];
-        int answer = result;
-        for (int i = 1; i < n; i++) {
-            result += arr[i];
-            answer += result;
+        int answer = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                // System.out.println("arr = " + arr[j] + " ");
+                answer += arr[j];
+            }
         }
         
         System.out.println(answer);
         
     }
 }
+    
+    
