8838472947addf94f85b62930bc50340ec27c0e5
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-04-17 23:58:50 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 160 ms, Memory: 22196 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -2,29 +2,25 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n, k, t, w;
-    public static int[] arr, tmp;
-    public static long[] dp;
+    public static int n, m, k, t, w;
+    public static long[] arr, tmp;
+    public static int[] dp;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
         StringTokenizer st = null;
         
-        n = Integer.parseInt(br.readLine());
-        arr = new int[1_000_001];
-        dp = new long[1_000_001];
-        
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
-        
+        t = Integer.parseInt(br.readLine());
+        arr = new long[1_000_001];
+        arr[1] = 1;
+        arr[2] = 2;
+        arr[3] = 4;
         for (int i = 4; i <= 1_000_000; i++) {
-            dp[i] = (dp[i - 3] + dp[i - 2] + dp[i - 1]) % 1_000_000_009;
+            arr[i] = (arr[i - 3] + arr[i - 2] + arr[i - 1]) % 1_000_000_009;
         }
-        
-        for (int i = 1; i <= n; i++) {
-            int num = Integer.parseInt(br.readLine());
-            System.out.println(dp[num] % 1_000_000_009);
+        for (int count = 0; count < t; count++) {
+            n = Integer.parseInt(br.readLine());
+            System.out.println(arr[n]);
         }
         
     }
