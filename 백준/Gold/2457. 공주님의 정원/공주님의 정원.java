c1c0a6a07ec18557cb07ef4c845d9560c2ec1d39
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-04-18 12:01:39 +0900

    [Gold III] Title: 공주님의 정원, Time: 544 ms, Memory: 53496 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
@@ -2,13 +2,16 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static int n, m, k, t, w;
+    public static int[][] arr, tmp;
+    public static int[] dp;
     
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
         StringTokenizer st = null;
         
-        int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][2];
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][2];
         
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -27,50 +30,35 @@ public class Main {
             if (o1[0] == o2[0]) {
                 return o2[1] - o1[1];
             }
+            
             return o1[0] - o2[0];
         });
         
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = 0; j < 2; j++) {
-        //         System.out.print(arr[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-        
         int start = 301;
-        int endDay = 1201;
-        int max = 0;
+        int end = 1201;
         int index = 0;
-        int count = 0;
+        int answer = 0;
         
-        while (start < endDay) {
-            boolean isFind = false;
-            
+        while (start < end) {
+            int next = start;
             for (int i = index; i < n; i++) {
-                if (arr[i][0] > start) {
-                    break;
-                }
-                
-                if (max < arr[i][1]) {
-                    isFind = true;
-                    max = arr[i][1];
+                if (arr[i][0] <= start && arr[i][1] > next) {
+                    next = arr[i][1];
                     index = i + 1;
                 }
             }
             
-            if (isFind) {
-                start = max;
-                count++;
-            } else {
-                break;
+            if (next == start) {
+                System.out.println(0);
+                return;
             }
+            
+            answer++;
+            start = next;
         }
         
-        if (max < endDay) {
-            System.out.println(0);
-        } else {
-            System.out.println(count);
-        }
-        
+        System.out.println(answer);
     }
 }
+    
+    
