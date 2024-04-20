fa7f53b452afb162ef7cb888c227e285c8958adf
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-04-21 00:59:37 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 552 ms, Memory: 27284 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
@@ -1,53 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static int n, m;
-    public static int x, y;
-
-    public static Long[] arr;
-
+    public static int n, m, k, t, w;
+    public static long[] arr, tmp;
+    public static int[] dp;
+    
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
+        StringTokenizer st = null;
+        
+        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
+        arr = new long[n];
+        
         st = new StringTokenizer(br.readLine());
-
-        arr = new Long[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = Long.parseLong(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
-
-
-//        while (true) {
-//            Arrays.sort(arr);
-//            Long newValue = arr[0]+ arr[1];
-//            arr[0] = newValue;
-//            arr[1] = newValue;
-//            m--;
-//            if (m == 0) {
-//                break;
-//            }
-//        }
+        
+        
         for (int i = 0; i < m; i++) {
             Arrays.sort(arr);
-            Long newValue = arr[0] + arr[1];
-            arr[0] = newValue;
-            arr[1] = newValue;
+            long sum = arr[0] + arr[1];
+            arr[0] = sum;
+            arr[1] = sum;
         }
-
-        Long result = 0L;
-        for (Long i : arr) {
-            result += i;
+        
+        long answer = 0;
+        for (long i : arr) {
+            answer += i;
         }
-
-        System.out.println(result);
+        
+        System.out.println(answer);
+        
     }
 }
+    
+    
