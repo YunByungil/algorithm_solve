810fe634f6267caabe6c9bc34d4be79e3639dab1
윤병일 <109706328+YunByungil@users.noreply.github.com>
2024-09-05 21:35:10 +0900

    [Bronze I] Title: 세로읽기, Time: 104 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -2,31 +2,45 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n, r, c, answer;
-    public static int[][] arr;
-    public static boolean[] visit;
-    public static StringBuilder sb = new StringBuilder();
+    public static int n, m, k, t, w, answer;
+    public static int f, s, g, u, d;
+    public static int[] dx = {0, 0, -1, 1};
+    public static int[] dy = {-1, 1, 0, 0};
+    // public static int[] dx = {1, 2, 1, 2, -1, -2, -1, -2};
+    // public static int[] dy = {-2, -1, 2, 1, -2, -1, 2, 1};
+    // public static int[][] arr;
+    public static String[][] arr;
+    public static boolean[][] visit;
+    // public static int[] arr;
+    // public static boolean[] visit;
+    public static Queue<int[]> q = new LinkedList<>();
+    // public static Queue<Integer> q = new LinkedList<>();
+    // public static List<Integer> list = new ArrayList<>();
     
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[][] input = new char[5][15]; 
- 
-        for (int i = 0; i < input.length; i++) {
-            String str = br.readLine();
-            
-            for (int j = 0; j < str.length(); j++) {
-                input[i][j] = str.charAt(j);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
+        StringTokenizer st = null;
+
+        arr = new String[5][15];
+        
+        for (int i = 0; i < 5; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < input.length(); j++) {
+                arr[i][j] = input.charAt(j) + "";
             }
         }
- 
+        
         for (int i = 0; i < 15; i++) {
             for (int j = 0; j < 5; j++) {
-                if (input[j][i] == '\0') {
+                if (arr[j][i] == null) {
+                    System.out.print("");
                     continue;
                 }
-                
-                System.out.print(input[j][i]);
+                System.out.print(arr[j][i]);
             }
         }
+        
     }
-}    
+}
+    
+    
