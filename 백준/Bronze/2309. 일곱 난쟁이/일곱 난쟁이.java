fc9291f370ea04894458dd4346942d0c877596c9
윤병일 <109706328+YunByungil@users.noreply.github.com>
2025-02-11 20:35:58 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 104 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,43 +1,57 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-        
-        int[] arr = new int[9];
-        int sum = 0;
-        for (int i = 0; i < 9; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            sum += arr[i];
+        int n = 9;
+        int max = 0;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            arr[i] = num;
+            max += num;
         }
-        Arrays.sort(arr);
-        boolean doing = true;
-        int x = 0;
-        int y = 0;
+
+        int start = 0;
+        int end = 0;
+        int sIndex = 0;
+        int eIndex = 0;
+        int result = 0;
         for (int i = 0; i < 8; i++) {
-            if (!doing) {
-                break;
-            }
+            start = arr[i];
+            result = 0;
             for (int j = i + 1; j < 9; j++) {
-                sum -= (arr[i] + arr[j]);
-                if (sum == 100) {
-                    x = i;
-                    y = j;
-                    doing = false;
+                end = arr[j];
+                int num = start + end;
+
+                result = max - num;
+                if (result == 100) {
+                    sIndex = i;
+                    eIndex = j;
                     break;
-                } else {
-                    sum += (arr[i] + arr[j]);
                 }
             }
+            if (result == 100) {
+                break;
+            }
         }
-        
-        for (int i = 0; i < 9; i++) {
-            if (i == x || i == y) {
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (i == sIndex || i == eIndex) {
                 continue;
             }
-            System.out.println(arr[i]);
+            list.add(arr[i]);
         }
+
+        Collections.sort(list);
+        for (Integer i : list) {
+            System.out.println(i);
+        }
+        
+        
     }
 }
+    
+    
