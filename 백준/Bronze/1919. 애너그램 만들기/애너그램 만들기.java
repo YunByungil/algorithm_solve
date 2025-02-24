0bed8c54764d6512bf08266e4e3c23b27ba38eb5
윤병일 <109706328+YunByungil@users.noreply.github.com>
2025-02-24 22:56:28 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,30 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static int[][] arr = new int[2][26];
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+        
+        // occurs 영단어와 succor는 서로 애너그램 관계
+        
+        // aabbcc, xxyybb가 존재할 때
+        // 애너그램 관계를 만들기 위해서는 aa, cc, xx, yy
+        // 따라서 총 8개를 제거해야 된다.
+        
         String str1 = br.readLine();
         String str2 = br.readLine();
-
+        
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+        
         for (int i = 0; i < str1.length(); i++) {
-            int index = str1.charAt(i) - 'a';
-            arr[0][index]++;
+            int num = str1.charAt(i) - 'a';
+            arr1[num]++;
         }
-
+        
         for (int i = 0; i < str2.length(); i++) {
-            int index = str2.charAt(i) - 'a';
-            arr[1][index]++;
+            int num = str2.charAt(i) - 'a';
+            arr2[num]++;
         }
-
-        int result = 0;
+        
+        int answer = 0;
         for (int i = 0; i < 26; i++) {
-            result += Math.abs(arr[0][i] - arr[1][i]);
+            if (arr1[i] != arr2[i]) {
+                int result = Math.abs(arr1[i] - arr2[i]);
+                answer += result;
+            }
         }
-
-        System.out.println(result);
+        
+        System.out.println(answer);
     }
-}
+}   
