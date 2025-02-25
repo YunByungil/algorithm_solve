58ff7b74c71e94a1d9aa9b4fa8b2fcdb41bfe96f
윤병일 <109706328+YunByungil@users.noreply.github.com>
2025-02-25 19:19:18 +0900

    [Silver IV] Title: 제로, Time: 212 ms, Memory: 23792 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -5,11 +5,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
         
-        int k = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
         
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(br.readLine());
             
             if (num == 0) {
@@ -20,7 +21,7 @@ public class Main {
         }
         
         int answer = 0;
-        for (int i : stack) {
+        for (Integer i : stack) {
             answer += i;
         }
         
