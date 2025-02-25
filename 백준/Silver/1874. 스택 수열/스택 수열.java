91b5a87ce3f67087eaba4f3945cf0a27a8f6d912
윤병일 <109706328+YunByungil@users.noreply.github.com>
2025-02-25 19:25:49 +0900

    [Silver II] Title: 스택 수열, Time: 308 ms, Memory: 27764 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -5,55 +5,34 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
-        
-        Stack<Integer> stack = new Stack<>();
-        int n = Integer.parseInt(br.readLine());
-        int max = 0;
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
+        
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+        
+        for (int count = 0; count < n; count++) {
             int num = Integer.parseInt(br.readLine());
-            // if (stack.isEmpty()) {
-            //     for (int j = 1; j <= num; j++) {
-            //         System.out.println("j = " + j);
-            //         stack.push(j);
-            //         sb.append("+").append("\n");
-            //     }
-            //     stack.pop();
-            //     sb.append("-").append("\n");
-            // } else if (stack.peek().equals(num)) {
-            //     stack.pop();
-            //     sb.append("-").append("\n");
-            // } else {
-            //     if (num > max) {
-            //         for (int j = max + 1; j <= num; j++) {
-            //             stack.push(j);
-            //             sb.append("+").append("\n");
-            //         }
-            //         stack.pop();
-            //         sb.append("-").append("\n");
-            //     }
-            // }
-            
-            if (!stack.isEmpty() && stack.peek().equals(num)) {
+            if (max < num) {
+                for (int i = max + 1; i <= num; i++) {
+                    stack.push(i);
+                    sb.append("+").append("\n");
+                }
+                max = num;
                 stack.pop();
                 sb.append("-").append("\n");
             } else {
-                if (num > max) {
-                    for (int j = max + 1; j <= num; j++) {
-                        stack.push(j);
-                        sb.append("+").append("\n");
-                    }
+                if (stack.peek() == num) {
                     stack.pop();
                     sb.append("-").append("\n");
+                } else {
+                    System.out.println("NO");
+                    return;
                 }
             }
-            max = Math.max(max, num);
         }
         
-        if (stack.isEmpty()) {
-            System.out.println(sb);
-        } else {
-            System.out.println("NO");
-        }
+        System.out.println(sb);
+        
     }
 }
