ac0d02059ea5ee147177ab9d9cad18d76f390a2b
윤병일 <109706328+YunByungil@users.noreply.github.com>
2025-02-28 20:17:07 +0900

    [Silver IV] Title: 카드2, Time: 156 ms, Memory: 45484 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,23 +1,34 @@
 import java.util.*;
 import java.io.*;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
         
-        Queue<Integer> q = new LinkedList<>();
         int n = Integer.parseInt(br.readLine());
+        
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+        Queue<Integer> q = new LinkedList<>();
         
         for (int i = 1; i <= n; i++) {
             q.offer(i);
         }
+
         
-        while (q.size() > 1) {
+        
+        while(q.size() > 2) {
             q.poll();
             q.offer(q.poll());
         }
         
-        System.out.println(q.poll());
+        q.poll();
+        System.out.println(q.peek());
+        
     }
 }
