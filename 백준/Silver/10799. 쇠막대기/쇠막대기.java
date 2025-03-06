34a1b76a41feba1f1533476ddfcefa8b86a7db81
윤병일 <109706328+YunByungil@users.noreply.github.com>
2025-03-06 23:51:30 +0900

    [Silver II] Title: 쇠막대기, Time: 152 ms, Memory: 16392 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,32 +1,35 @@
 import java.util.*;
 import java.io.*;
 
+
 public class Main {
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
         
-        Stack<Character> stack = new Stack<>();
-        int answer = 0;
-        
+        Stack<Character> s = new Stack<>();
         String input = br.readLine();
+        int answer = 0;
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             
             if (c == '(') {
-                stack.push(c);
-            } else {
-                stack.pop();
-                
+                s.push(c);
+            }
+            
+            if (c == ')') {
+                s.pop();
                 if (input.charAt(i - 1) == ')') {
                     answer++;
                 } else {
-                    answer += stack.size();
+                    answer += s.size();
                 }
             }
+            
         }
         
         System.out.println(answer);
+        
     }
 }
